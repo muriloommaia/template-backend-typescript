4f2655750a684fd6ae78495308c74e5bcefdb0fa
MuriloM <muriloommaia@gmail.com>
2022-02-25 14:48:20 -0300

    test

---
+++ tests/test.test.ts
@@ -0,0 +1,8 @@
+import { helloWorld } from '../src'
+
+describe('Teste', () => {
+  it('Test index.ts', () => {
+    const response = 'Hello World'
+    expect(helloWorld()).toBe(response)
+  })
+})
